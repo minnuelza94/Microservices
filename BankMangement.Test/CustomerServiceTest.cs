c9b6a14ab4334f77853087819da3fc5b284ba684
User <User@DEV-420192616-0>
2022-05-10 15:46:15 +0530

    Modify99

--- BankMangement.Test/CustomerServiceTest.cs
+++ BankMangement.Test/CustomerServiceTest.cs
@@ -2,7 +2,6 @@
 using BankManagementMicroservice.Service.Model;
 using BankMangementMicroservice.Data.Repository;
 using BankMangementMicroservice.Service.Service;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
@@ -19,8 +18,10 @@ namespace BankMangement.Test
         private Mock<IMapper> _mapper;
         private Mock<ILogger> _logger;
         public customerEntity _customer;
+        public customerEntity _customer1;
         public loanEntity _loan;
         public RegisterModel _registerModel;
+        public CustomerDetail _customerModel;
 
         [SetUp]
         public void Setup()
@@ -31,11 +32,13 @@ namespace BankMangement.Test
             _logger = new Mock<ILogger>();
             _loanRepository = new Mock<ILoanRepository>();
             _customer = new customerEntity() { Name = "anna", Username = "anna", Password = "password1", ContactNo = "9600209432", EmailAddress = "anna@anna.com", Address = "Changanassery", Country = "India", State = "Kerala", AccountType = "Savings", DOB = Convert.ToDateTime("13-07-1994"), CreatedOnUtc = DateTime.UtcNow, PAN = "BPFPN9256N" };
+            _customer1 = new customerEntity() { CustomerId = 1,Name = "anna", Username = "anna", Password = "password1", ContactNo = "9600209432", EmailAddress = "anna@anna.com", Address = "Changanassery", Country = "India", State = "Kerala", AccountType = "Savings", DOB = Convert.ToDateTime("13-07-1994"), CreatedOnUtc = DateTime.UtcNow, PAN = "BPFPN9256N" };
             _loan = new loanEntity() { CustomerId = 1, LoanType = "Personal", LoanAmount = 200000, Date = DateTime.UtcNow, DurationofLoan = 24, RateofInterest = 7 };
             _registerModel = new RegisterModel() { Name = "anna", Username = "anna", Password = "password1", ContactNo = "9600209432", EmailAddress = "anna@anna.com", Address = "Changanassery", Country = "India", State = "Kerala", AccountType = "Savings", DOB = Convert.ToDateTime("13-07-1994"), CreatedOnUtc = DateTime.UtcNow, PAN = "BPFPN9256N" };
+            _customerModel = new CustomerDetail() { Name = "anna", Username = "anna", Password = "password1", ContactNo = "9600209432", EmailAddress = "anna@anna.com", Address = "Changanassery", Country = "India", State = "Kerala", AccountType = "Savings", DOB = Convert.ToDateTime("13-07-1994"), CreatedOnUtc = DateTime.UtcNow, PAN = "BPFPN9256N" };
 
         }
-        #region Validations
+
 
 
         [Test]
@@ -51,76 +54,26 @@ namespace BankMangement.Test
             var actionResult = service.CreateCustomer(_registerModel);
 
             // Assert
-            Assert.IsInstanceOf<OkObjectResult>(actionResult);
             Assert.IsNotNull(actionResult);
         }
 
-        [Test]
-        public void AddCustomer_ThrowsException()
-        {
-            // Arrange
-            _customerService.Setup(x => x.CreateNewCustomer(_customer)).Throws(new Exception());
-            var controller = new BankController(_customerService.Object, _loanService.Object);
-
-            // Assert
-            Assert.Throws<Exception>(() => controller.AddCustomer(_customer));
-        }
-
+       
         [Test]
         public void UpdateCustomer_OkResult()
         {
             // Arrange
-            _customerService.Setup(x => x.UpdateCustomerInfo(_customer));
+            _customerRepository.Setup(x => x.GetCustomerById(1)).ReturnsAsync(_customer1);
+            _customerRepository.Setup(x => x.UpdateCustomer(_customer1));
+            
 
-            var controller = new BankController(_customerService.Object, _loanService.Object);
-
-            // Act
-            var actionResult = controller.UpdateCustomer(_customer);
-
-            // Assert
-            Assert.IsInstanceOf<OkObjectResult>(actionResult);
-            Assert.IsNotNull(actionResult);
-        }
-
-        [Test]
-        public void Updatecustomer_ThorowsException()
-        {
-            // Arrange
-            _customerService.Setup(x => x.UpdateCustomerInfo(_customer)).Throws(new Exception());
-
-            var controller = new BankController(_customerService.Object, _loanService.Object);
-
-            // Assert
-            Assert.Throws<Exception>(() => controller.UpdateCustomer(_customer));
-        }
-
-        [Test]
-        public void ApplyLoan_OkResult()
-        {
-            // Arrange
-            _loanService.Setup(x => x.ApplyLoan(_loan));
-
-            var controller = new BankController(_customerService.Object, _loanService.Object);
+            var service = new  CustomerService(_customerRepository.Object, _mapper.Object, _logger.Object);
 
             // Act
-            var actionResult = controller.ApplyLoan(_loan);
+            _customerModel.CustomerId = 1;
+            var actionResult = service.UpdateCustomer(_customerModel);
 
-            // Assert
-            Assert.IsInstanceOf<OkObjectResult>(actionResult);
+            // Assert           
             Assert.IsNotNull(actionResult);
         }
-
-        [Test]
-        public void Updatecustomer_ThrowsException()
-        {
-            // Arrange
-            _loanService.Setup(x => x.ApplyLoan(_loan)).Throws(new Exception());
-
-            var controller = new BankController(_customerService.Object, _loanService.Object);
-
-            // Assert
-            Assert.Throws<Exception>(() => controller.ApplyLoan(_loan));
-        }
-        #endregion
     }
 }
